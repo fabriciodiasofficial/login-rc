522dffc10b176f621393914f10662c26cdcab19c
FABRICIO DIAS <fabriciodias97.ti@gmail.com>
2024-02-15 12:05:58 -0200

    Subindo front-end 1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<div class="box_login">
+		<div class="capa">
+		</div>
+
+		<h1>Faça seu login</h1>
+
+		<form>
+			<input type="email" name="e-mail">
+			<input type="password" name="password">
+
+			<button class="btn-login">LOGAR</button>
+		</form>
+	</div>
+</body>
+</html>
