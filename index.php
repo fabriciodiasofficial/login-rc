1096b02cdd2775b0a568bce0c51f355fb586a8ee
FABRICIO DIAS <fabriciodias97.ti@gmail.com>
2024-02-16 11:25:44 -0200

    Iniciando o back-end

--- index.php
+++ index.php
@@ -1,34 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<?php 
+	
+	require_once 'app/core/Core.php';
 
-<script src="https://kit.fontawesome.com/59b638ad9f.js" crossorigin="anonymous"></script>
-</head>
-<body>
+	$core = new Core;
+	$core->start($_GET);
 
-	<div class="box_login">
-		<div class="capa">
-		</div>
-
-		<h1>Faça seu login</h1>
-
-		<form>
-			<input type="email" name="e-mail" placeholder="NOME COMPLETO">
-			<input type="password" name="password" placeholder="E-MAIL">
-
-			<!-- {% if error != '' %}
-				<p id="msg_error" style="text-align: center; font-size: 12px; margin-bottom: 5px; color: orange;">
-						{{error}}<i class='fa fa-exclamation'></i>
-				</p>
-			{% endif %} -->
-
-			<button class="btn-login">LOGAR</button>
-			<span class="msg_error"><i class="fa-solid fa-circle-exclamation" style="font-size:16px; color:#FF6D6D; padding-right: 5px"></i><p>Tentativa Inválida</p></span>
-		</form>
-	</div>
-</body>
-</html>
+ ?>
